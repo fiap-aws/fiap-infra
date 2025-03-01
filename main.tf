a0b89c7c03ddea802fe3cfe277811af033f0ea55
Eduardo Munari <munari.edu@gmail.com>
2025-03-01 12:03:23 -0300

    chore: enable pipeline and remote state

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    region = "us-east-1"
+    bucket = "fiap-devops-statebucket"
+    key    = "terraform.tfstate"
+  }
 }
 
 provider "aws" {
