e6faf76a17b7324ac0866f31bb017c450dc85ce6
LorenaBastos <lorenabastos066@gmail.com>
2025-03-01 12:19:53 -0300

    Add EC2, ECS and load balancer services

---
+++ resources/ec2.tf
@@ -0,0 +1,46 @@
+# AWS Launch Template
+resource "aws_launch_template" "fiap_devops_ecs_lt" {
+ name_prefix   = "ecs-template"
+ image_id      = "ami-05b10e08d247fb927"
+ instance_type = "t2.micro"
+
+ key_name               = "fiap_keypair"
+ vpc_security_group_ids = [aws_security_group.fiap_devops_security_group.id]
+ iam_instance_profile {
+   name = "ecsInstanceRole"
+ }
+
+ block_device_mappings {
+   device_name = "/dev/xvda"
+   ebs {
+     volume_size = 30
+     volume_type = "gp2"
+   }
+ }
+
+ tag_specifications {
+   resource_type = "instance"
+   tags = {
+     Name = "ecs-instance"
+   }
+ }
+}
+
+resource "aws_autoscaling_group" "fiap_devops_ecs_asg" {
+ vpc_zone_identifier = [aws_subnet.fiap_devops_public_subnet.id]
+ desired_capacity    = 1
+ max_size            = 1
+ min_size            = 1
+
+ launch_template {
+   id      = aws_launch_template.ecs_lt.id
+   version = "$Latest"
+ }
+
+ tag {
+   key                 = "AmazonECSManaged"
+   value               = true
+   propagate_at_launch = true
+ }
+}
+
--- resources/ecs.tf
+++ resources/ecs.tf
@@ -1,1 +1,64 @@
-#Example
+resource "aws_ecs_cluster" "fiap_devops_ecs_cluster" {
+ name = "my-ecs-cluster"
+}
+
+resource "aws_ecs_capacity_provider" "ecs_capacity_provider" {
+ name = "fiap_devops_capacity_ecs"
+
+ auto_scaling_group_provider {
+   auto_scaling_group_arn = aws_autoscaling_group.fiap_devops_ecs_asg.arn
+
+   managed_scaling {
+     maximum_scaling_step_size = 1000
+     minimum_scaling_step_size = 1
+     status                    = "ENABLED"
+     target_capacity           = 3
+   }
+ }
+}
+
+resource "aws_ecs_cluster_capacity_providers" "fiap_devops_ecs_cluster_capacity" {
+ cluster_name = aws_ecs_cluster.ecs_cluster.name
+
+ capacity_providers = [aws_ecs_capacity_provider.ecs_capacity_provider.name]
+
+ default_capacity_provider_strategy {
+   base              = 1
+   weight            = 100
+   capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
+ }
+}
+
+resource "aws_ecs_service" "ecs_service" {
+ name            = "my-ecs-service"
+ cluster         = aws_ecs_cluster.ecs_cluster.id
+ task_definition = aws_ecs_task_definition.ecs_task_definition.arn
+ desired_count   = 2
+
+ network_configuration {
+   subnets         = [aws_subnet.fiap_devops_public_subnet.id]
+   security_groups = [aws_security_group.fiap_devops_security_group.id]
+ }
+
+ force_new_deployment = true
+ placement_constraints {
+   type = "distinctInstance"
+ }
+
+ triggers = {
+   redeployment = timestamp()
+ }
+
+ capacity_provider_strategy {
+   capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
+   weight            = 100
+ }
+
+ load_balancer {
+   target_group_arn = aws_lb_target_group.fiap_devops_ecs_tg.arn
+   container_name   = "fiap_devops_app"
+   container_port   = 80
+ }
+
+ depends_on = [aws_autoscaling_group.fiap_devops_ecs_asg]
+}
