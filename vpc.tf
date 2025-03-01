41660cbfcfb640d53fa793a3128bb420c45855ce
LorenaBastos <lorenabastos066@gmail.com>
2025-03-01 18:38:30 -0300

    Add ingress rule

--- vpc.tf
+++ vpc.tf
@@ -77,3 +77,10 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
 }
+
+#Ingress Rule for RG
+resource "aws_vpc_security_group_ingress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.fiap_devops_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
