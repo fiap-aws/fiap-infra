1c01a0cd43f0f7abc60e3437c9199cea57420938
Eduardo Munari <munari.edu@gmail.com>
2025-03-01 21:16:52 -0300

    chore: create network resources

---
+++ network.tf
@@ -0,0 +1,49 @@
+data "aws_availability_zones" "available" { state = "available" }
+
+locals {
+  azs_count = 2
+  azs_names = data.aws_availability_zones.available.names
+}
+
+resource "aws_vpc" "fiap_devops_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags                 = { Name = "fiap-devops-vpc" }
+}
+
+resource "aws_subnet" "fiap_devops_public_subnet" {
+  count                   = local.azs_count
+  vpc_id                  = aws_vpc.fiap_devops_vpc.id
+  availability_zone       = local.azs_names[count.index]
+  cidr_block              = cidrsubnet(aws_vpc.fiap_devops_vpc.cidr_block, 8, 10 + count.index)
+  map_public_ip_on_launch = true
+  tags                    = { Name = "fiap-devops-public-subnet-${local.azs_names[count.index]}"}
+}
+
+resource "aws_internet_gateway" "fiap_devops_igw" {
+  vpc_id = aws_vpc.fiap_devops_vpc.id
+  tags   = { Name = "fiap-devops-igw" }
+}
+
+resource "aws_eip" "fiap_devops_eip" {
+  count      = local.azs_count
+  depends_on = [aws_internet_gateway.fiap_devops_igw]
+  tags       = { Name = "fiap-devops-eip-${local.azs_names[count.index]}" }
+}
+
+resource "aws_route_table" "fiap_devops_rt" {
+  vpc_id = aws_vpc.fiap_devops_vpc.id
+  tags   = { Name = "fiap-devops-rt-public" }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.fiap_devops_igw.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = local.azs_count
+  subnet_id      = aws_subnet.fiap_devops_public_subnet[count.index].id
+  route_table_id = aws_route_table.fiap_devops_rt.id
+}
