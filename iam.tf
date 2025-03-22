9c0adacff7424690ccae2aa7b157324fe66a24f9
LorenaBastos <lorenabastos066@gmail.com>
2025-03-22 16:09:00 -0300

    chore: delete all AWS resources

--- iam.tf
+++ iam.tf
@@ -1,58 +1,58 @@
-# --- ECS Node Role ---
+# # --- ECS Node Role ---
 
-data "aws_iam_policy_document" "fiap_devops_ecs_node_doc" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    effect  = "Allow"
+# data "aws_iam_policy_document" "fiap_devops_ecs_node_doc" {
+#   statement {
+#     actions = ["sts:AssumeRole"]
+#     effect  = "Allow"
 
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
+#     principals {
+#       type        = "Service"
+#       identifiers = ["ec2.amazonaws.com"]
+#     }
+#   }
+# }
 
-resource "aws_iam_role" "fiap_devops_ecs_node_role" {
-  name_prefix        = "fiap-devops-ecs-node-role"
-  assume_role_policy = data.aws_iam_policy_document.fiap_devops_ecs_node_doc.json
-}
+# resource "aws_iam_role" "fiap_devops_ecs_node_role" {
+#   name_prefix        = "fiap-devops-ecs-node-role"
+#   assume_role_policy = data.aws_iam_policy_document.fiap_devops_ecs_node_doc.json
+# }
 
-resource "aws_iam_role_policy_attachment" "fiap_devops_ecs_node_role_policy" {
-  role       = aws_iam_role.fiap_devops_ecs_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
-}
+# resource "aws_iam_role_policy_attachment" "fiap_devops_ecs_node_role_policy" {
+#   role       = aws_iam_role.fiap_devops_ecs_node_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+# }
 
-resource "aws_iam_instance_profile" "fiap_devops_ecs_node" {
-  name_prefix = "fiap-devops-ecs-node-profile"
-  path        = "/ecs/instance/"
-  role        = aws_iam_role.fiap_devops_ecs_node_role.name
-}
+# resource "aws_iam_instance_profile" "fiap_devops_ecs_node" {
+#   name_prefix = "fiap-devops-ecs-node-profile"
+#   path        = "/ecs/instance/"
+#   role        = aws_iam_role.fiap_devops_ecs_node_role.name
+# }
 
-# --- ECS Task Role ---
+# # --- ECS Task Role ---
 
-data "aws_iam_policy_document" "fiap_devops_ecs_task_doc" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    effect  = "Allow"
+# data "aws_iam_policy_document" "fiap_devops_ecs_task_doc" {
+#   statement {
+#     actions = ["sts:AssumeRole"]
+#     effect  = "Allow"
 
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
+#     principals {
+#       type        = "Service"
+#       identifiers = ["ecs-tasks.amazonaws.com"]
+#     }
+#   }
+# }
 
-resource "aws_iam_role" "fiap_devops_ecs_task_role" {
-  name_prefix        = "fiap-devops-ecs-task-role"
-  assume_role_policy = data.aws_iam_policy_document.fiap_devops_ecs_task_doc.json
-}
+# resource "aws_iam_role" "fiap_devops_ecs_task_role" {
+#   name_prefix        = "fiap-devops-ecs-task-role"
+#   assume_role_policy = data.aws_iam_policy_document.fiap_devops_ecs_task_doc.json
+# }
 
-resource "aws_iam_role" "fiap_devops_ecs_exec_role" {
-  name_prefix        = "fiap-devops-ecs-exec-role"
-  assume_role_policy = data.aws_iam_policy_document.fiap_devops_ecs_task_doc.json
-}
+# resource "aws_iam_role" "fiap_devops_ecs_exec_role" {
+#   name_prefix        = "fiap-devops-ecs-exec-role"
+#   assume_role_policy = data.aws_iam_policy_document.fiap_devops_ecs_task_doc.json
+# }
 
-resource "aws_iam_role_policy_attachment" "fiap_devops_ecs_exec_role_policy" {
-  role       = aws_iam_role.fiap_devops_ecs_exec_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
+# resource "aws_iam_role_policy_attachment" "fiap_devops_ecs_exec_role_policy" {
+#   role       = aws_iam_role.fiap_devops_ecs_exec_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+# }
--- network.tf
+++ network.tf
@@ -1,111 +1,111 @@
-data "aws_availability_zones" "available" { state = "available" }
+# data "aws_availability_zones" "available" { state = "available" }
 
-locals {
-  azs_count = 2
-  azs_names = data.aws_availability_zones.available.names
-}
+# locals {
+#   azs_count = 2
+#   azs_names = data.aws_availability_zones.available.names
+# }
 
-resource "aws_vpc" "fiap_devops_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  tags                 = { Name = "fiap-devops-vpc" }
-}
+# resource "aws_vpc" "fiap_devops_vpc" {
+#   cidr_block           = "10.0.0.0/16"
+#   enable_dns_hostnames = true
+#   enable_dns_support   = true
+#   tags                 = { Name = "fiap-devops-vpc" }
+# }
 
-resource "aws_subnet" "fiap_devops_public_subnet" {
-  count                   = local.azs_count
-  vpc_id                  = aws_vpc.fiap_devops_vpc.id
-  availability_zone       = local.azs_names[count.index]
-  cidr_block              = cidrsubnet(aws_vpc.fiap_devops_vpc.cidr_block, 8, 10 + count.index)
-  map_public_ip_on_launch = true
-  tags                    = { Name = "fiap-devops-public-subnet-${local.azs_names[count.index]}" }
-}
+# resource "aws_subnet" "fiap_devops_public_subnet" {
+#   count                   = local.azs_count
+#   vpc_id                  = aws_vpc.fiap_devops_vpc.id
+#   availability_zone       = local.azs_names[count.index]
+#   cidr_block              = cidrsubnet(aws_vpc.fiap_devops_vpc.cidr_block, 8, 10 + count.index)
+#   map_public_ip_on_launch = true
+#   tags                    = { Name = "fiap-devops-public-subnet-${local.azs_names[count.index]}" }
+# }
 
-resource "aws_internet_gateway" "fiap_devops_igw" {
-  vpc_id = aws_vpc.fiap_devops_vpc.id
-  tags   = { Name = "fiap-devops-igw" }
-}
+# resource "aws_internet_gateway" "fiap_devops_igw" {
+#   vpc_id = aws_vpc.fiap_devops_vpc.id
+#   tags   = { Name = "fiap-devops-igw" }
+# }
 
-resource "aws_eip" "fiap_devops_eip" {
-  count      = local.azs_count
-  depends_on = [aws_internet_gateway.fiap_devops_igw]
-  tags       = { Name = "fiap-devops-eip-${local.azs_names[count.index]}" }
-}
+# resource "aws_eip" "fiap_devops_eip" {
+#   count      = local.azs_count
+#   depends_on = [aws_internet_gateway.fiap_devops_igw]
+#   tags       = { Name = "fiap-devops-eip-${local.azs_names[count.index]}" }
+# }
 
-resource "aws_route_table" "fiap_devops_rt" {
-  vpc_id = aws_vpc.fiap_devops_vpc.id
-  tags   = { Name = "fiap-devops-rt-public" }
+# resource "aws_route_table" "fiap_devops_rt" {
+#   vpc_id = aws_vpc.fiap_devops_vpc.id
+#   tags   = { Name = "fiap-devops-rt-public" }
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.fiap_devops_igw.id
-  }
-}
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.fiap_devops_igw.id
+#   }
+# }
 
-resource "aws_route_table_association" "public" {
-  count          = local.azs_count
-  subnet_id      = aws_subnet.fiap_devops_public_subnet[count.index].id
-  route_table_id = aws_route_table.fiap_devops_rt.id
-}
+# resource "aws_route_table_association" "public" {
+#   count          = local.azs_count
+#   subnet_id      = aws_subnet.fiap_devops_public_subnet[count.index].id
+#   route_table_id = aws_route_table.fiap_devops_rt.id
+# }
 
-resource "aws_security_group" "fiap_devops_ecs_node_sg" {
-  name_prefix = "fiap-devops-ecs-node-sg-"
-  vpc_id      = aws_vpc.fiap_devops_vpc.id
-}
+# resource "aws_security_group" "fiap_devops_ecs_node_sg" {
+#   name_prefix = "fiap-devops-ecs-node-sg-"
+#   vpc_id      = aws_vpc.fiap_devops_vpc.id
+# }
 
-resource "aws_vpc_security_group_egress_rule" "allow_egress_ecs_node" {
-  security_group_id = aws_security_group.fiap_devops_ecs_node_sg.id
+# resource "aws_vpc_security_group_egress_rule" "allow_egress_ecs_node" {
+#   security_group_id = aws_security_group.fiap_devops_ecs_node_sg.id
 
-  cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 0
-  ip_protocol = "tcp"
-  to_port     = 65535
-}
+#   cidr_ipv4   = "0.0.0.0/0"
+#   from_port   = 0
+#   ip_protocol = "tcp"
+#   to_port     = 65535
+# }
 
-resource "aws_security_group" "fiap_devops_ecs_task_sg" {
-  name_prefix = "fiap-devops-ecs-task-sg-"
-  description = "Allow all traffic within the VPC"
-  vpc_id      = aws_vpc.fiap_devops_vpc.id
-}
+# resource "aws_security_group" "fiap_devops_ecs_task_sg" {
+#   name_prefix = "fiap-devops-ecs-task-sg-"
+#   description = "Allow all traffic within the VPC"
+#   vpc_id      = aws_vpc.fiap_devops_vpc.id
+# }
 
-resource "aws_vpc_security_group_egress_rule" "allow_egress_ecs_task" {
-  security_group_id = aws_security_group.fiap_devops_ecs_task_sg.id
+# resource "aws_vpc_security_group_egress_rule" "allow_egress_ecs_task" {
+#   security_group_id = aws_security_group.fiap_devops_ecs_task_sg.id
 
-  cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 0
-  ip_protocol = "tcp"
-  to_port     = 65535
-}
+#   cidr_ipv4   = "0.0.0.0/0"
+#   from_port   = 0
+#   ip_protocol = "tcp"
+#   to_port     = 65535
+# }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_ingress_ecs_task" {
-  security_group_id = aws_security_group.fiap_devops_ecs_task_sg.id
+# resource "aws_vpc_security_group_ingress_rule" "allow_ingress_ecs_task" {
+#   security_group_id = aws_security_group.fiap_devops_ecs_task_sg.id
 
-  cidr_ipv4   = aws_vpc.fiap_devops_vpc.cidr_block
-  from_port   = 0
-  ip_protocol = "tcp"
-  to_port     = 65535
-}
+#   cidr_ipv4   = aws_vpc.fiap_devops_vpc.cidr_block
+#   from_port   = 0
+#   ip_protocol = "tcp"
+#   to_port     = 65535
+# }
 
-resource "aws_security_group" "fiap_devops_alb_sg" {
-  name_prefix = "fiap-devops-alb-sg-"
-  description = "Allow all HTTP/HTTPS traffic from public"
-  vpc_id      = aws_vpc.fiap_devops_vpc.id
-}
+# resource "aws_security_group" "fiap_devops_alb_sg" {
+#   name_prefix = "fiap-devops-alb-sg-"
+#   description = "Allow all HTTP/HTTPS traffic from public"
+#   vpc_id      = aws_vpc.fiap_devops_vpc.id
+# }
 
-resource "aws_vpc_security_group_egress_rule" "allow_egress_alb" {
-  security_group_id = aws_security_group.fiap_devops_alb_sg.id
+# resource "aws_vpc_security_group_egress_rule" "allow_egress_alb" {
+#   security_group_id = aws_security_group.fiap_devops_alb_sg.id
 
-  cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 0
-  ip_protocol = "tcp"
-  to_port     = 65535
-}
+#   cidr_ipv4   = "0.0.0.0/0"
+#   from_port   = 0
+#   ip_protocol = "tcp"
+#   to_port     = 65535
+# }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_ingress_alb" {
-  security_group_id = aws_security_group.fiap_devops_alb_sg.id
+# resource "aws_vpc_security_group_ingress_rule" "allow_ingress_alb" {
+#   security_group_id = aws_security_group.fiap_devops_alb_sg.id
 
-  cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 80
-  ip_protocol = "tcp"
-  to_port     = 80
-}
+#   cidr_ipv4   = "0.0.0.0/0"
+#   from_port   = 80
+#   ip_protocol = "tcp"
+#   to_port     = 80
+# }
